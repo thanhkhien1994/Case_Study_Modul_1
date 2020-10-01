4c34e51df1f0d3945d26a5707634b598ff4939c5
KHIÊN-CODEGYM <you@example.com>
2020-10-01 12:01:45 +0700

    them file case study

---
+++ js/variable.js
@@ -0,0 +1,11 @@
+let objStudents = localStorage.getItem("savedData") ? JSON.parse(localStorage.getItem("savedData")) : JSON.stringify("Rỗng");
+
+let gender = "Nam";
+let _name = document.getElementById("name");
+let _class = document.getElementById("class");
+let _age = document.getElementById("age");
+let _address = document.getElementById("address");
+let _mail = document.getElementById("mail");
+let add = document.getElementById("add");
+let search = document.getElementById('search');
+let display = document.getElementById('display');
